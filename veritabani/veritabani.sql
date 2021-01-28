3c5c4ffaed4acd014caa6512e8439f5c670b5b07
Nusret YÜKSEL <nsrtyksl@ya.ru>
2021-01-29 00:30:40 +0300

    Update veritabani.sql
    Lisans tablosu kaldırıldı.
    Yönetici: admin Şifre: 123456 olarak değiştirildi.

--- veritabani/veritabani.sql
+++ veritabani/veritabani.sql
@@ -18,133 +18,124 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Veritabanı: `admingiris`
+-- Veritabanı: 'admingiris'
 --
 
 -- --------------------------------------------------------
 
 --
--- Tablo için tablo yapısı `accounts`
+-- Tablo için tablo yapısı 'accounts'
 --
 
-DROP TABLE IF EXISTS `accounts`;
-CREATE TABLE IF NOT EXISTS `accounts` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(50) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
+DROP TABLE IF EXISTS 'accounts';
+CREATE TABLE IF NOT EXISTS 'accounts' (
+  'id' int(11) NOT NULL AUTO_INCREMENT,
+  'username' varchar(50) NOT NULL,
+  'password' varchar(255) NOT NULL,
+  PRIMARY KEY ('id')
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 --
--- Tablo döküm verisi `accounts`
+-- Tablo döküm verisi 'accounts'
 --
 
-INSERT INTO `accounts` (`id`, `username`, `password`) VALUES
+INSERT INTO 'accounts' ('id', 'username', 'password') VALUES
 (1, 'test', '$2y$10$SfhYIDtn.iOuCW7zfoFLuuZHX6lja4lF4XA4JqNmpiH/.P3zB8JCa');
 
 -- --------------------------------------------------------
 
 --
--- Tablo için tablo yapısı `admin`
+-- Tablo için tablo yapısı 'admin'
 --
 
-DROP TABLE IF EXISTS `admin`;
-CREATE TABLE IF NOT EXISTS `admin` (
-  `id` int(255) NOT NULL,
-  `user` varchar(255) NOT NULL,
-  `pass` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
+DROP TABLE IF EXISTS 'admin';
+CREATE TABLE IF NOT EXISTS 'admin' (
+  'id' int(255) NOT NULL,
+  'user' varchar(255) NOT NULL,
+  'pass' varchar(255) NOT NULL,
+  PRIMARY KEY ('id')
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Tablo döküm verisi `admin`
+-- Tablo döküm verisi 'admin'
 --
 
-INSERT INTO `admin` (`id`, `user`, `pass`) VALUES
-(1, 'adminx', 'fe01ce2a7fbac8fafaed7c982a04e229');
+INSERT INTO 'admin' ('id', 'user', 'pass') VALUES
+(1, 'adminx', 'e10adc3949ba59abbe56e057f20f883e');
 
 -- --------------------------------------------------------
 
 --
--- Tablo için tablo yapısı `domainler`
+-- Tablo için tablo yapısı 'domainler'
 --
 
-DROP TABLE IF EXISTS `domainler`;
-CREATE TABLE IF NOT EXISTS `domainler` (
-  `id` int(255) NOT NULL AUTO_INCREMENT,
-  `domain` varchar(255) NOT NULL,
-  `k_tarih` varchar(255) NOT NULL,
-  `b_tarih` varchar(255) NOT NULL,
-  `s_tarih` varchar(255) NOT NULL,
-  `durum` varchar(255) NOT NULL,
-  `fiyat` int(255) NOT NULL,
-  PRIMARY KEY (`id`)
+DROP TABLE IF EXISTS 'domainler';
+CREATE TABLE IF NOT EXISTS 'domainler' (
+  'id' int(255) NOT NULL AUTO_INCREMENT,
+  'domain' varchar(255) NOT NULL,
+  'k_tarih' varchar(255) NOT NULL,
+  'b_tarih' varchar(255) NOT NULL,
+  's_tarih' varchar(255) NOT NULL,
+  'durum' varchar(255) NOT NULL,
+  'fiyat' int(255) NOT NULL,
+  PRIMARY KEY ('id')
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
 
 --
--- Tablo döküm verisi `domainler`
+-- Tablo döküm verisi 'domainler'
 --
 
-INSERT INTO `domainler` (`id`, `domain`, `k_tarih`, `b_tarih`, `s_tarih`, `durum`, `fiyat`) VALUES
+INSERT INTO 'domainler' ('id', 'domain', 'k_tarih', 'b_tarih', 's_tarih', 'durum', 'fiyat') VALUES
 (1, 'deneme.org', '2020-09-15', '2021-09-15', '2020-09-15', 'Pasif', 0);
 
 -- --------------------------------------------------------
 
 --
--- Tablo için tablo yapısı `license`
+-- Tablo için tablo yapısı 'license'
 --
 
-DROP TABLE IF EXISTS `license`;
-CREATE TABLE IF NOT EXISTS `license` (
-  `license_no` varchar(255) NOT NULL,
-  `domain` varchar(255) NOT NULL
+DROP TABLE IF EXISTS 'license';
+CREATE TABLE IF NOT EXISTS 'license' (
+  'license_no' varchar(255) NOT NULL,
+  'domain' varchar(255) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 --
--- Tablo döküm verisi `license`
+-- Tablo için tablo yapısı 'siparis'
 --
 
-INSERT INTO `license` (`license_no`, `domain`) VALUES
-('ECT-305925665\r\n', 'localhost');
-
--- --------------------------------------------------------
-
---
--- Tablo için tablo yapısı `siparis`
---
-
-DROP TABLE IF EXISTS `siparis`;
-CREATE TABLE IF NOT EXISTS `siparis` (
-  `id` int(255) NOT NULL AUTO_INCREMENT,
-  `ad` varchar(255) NOT NULL,
-  `soyad` varchar(255) NOT NULL,
-  `domain` varchar(255) NOT NULL,
-  `fiyat` int(255) NOT NULL,
-  PRIMARY KEY (`id`)
+DROP TABLE IF EXISTS 'siparis';
+CREATE TABLE IF NOT EXISTS 'siparis' (
+  'id' int(255) NOT NULL AUTO_INCREMENT,
+  'ad' varchar(255) NOT NULL,
+  'soyad' varchar(255) NOT NULL,
+  'domain' varchar(255) NOT NULL,
+  'fiyat' int(255) NOT NULL,
+  PRIMARY KEY ('id')
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
 --
--- Tablo için tablo yapısı `site`
+-- Tablo için tablo yapısı 'site'
 --
 
-DROP TABLE IF EXISTS `site`;
-CREATE TABLE IF NOT EXISTS `site` (
-  `id` int(255) NOT NULL AUTO_INCREMENT,
-  `title` varchar(255) NOT NULL,
-  `meta` varchar(255) NOT NULL,
-  `keywords` varchar(255) NOT NULL,
-  `descp` varchar(255) NOT NULL,
-  `theme` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
+DROP TABLE IF EXISTS 'site';
+CREATE TABLE IF NOT EXISTS 'site' (
+  'id' int(255) NOT NULL AUTO_INCREMENT,
+  'title' varchar(255) NOT NULL,
+  'meta' varchar(255) NOT NULL,
+  'keywords' varchar(255) NOT NULL,
+  'descp' varchar(255) NOT NULL,
+  'theme' varchar(255) NOT NULL,
+  PRIMARY KEY ('id')
 ) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 --
--- Tablo döküm verisi `site`
+-- Tablo döküm verisi 'site'
 --
 
-INSERT INTO `site` (`id`, `title`, `meta`, `keywords`, `descp`, `theme`) VALUES
+INSERT INTO 'site' ('id', 'title', 'meta', 'keywords', 'descp', 'theme') VALUES
 (1, 'Deneme Site', 'Denem sitesi test modu.', 'deneme, 123', 'deneme, 123', 'master');
 COMMIT;
 
